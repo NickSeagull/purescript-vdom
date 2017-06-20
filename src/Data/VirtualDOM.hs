b76f0bd8062abe92b6a834d4402c4c44d7a4dd79
Nikita Tchayka <nikitatchayka@gmail.com>
2017-06-20 10:49:26 +0100

    Add Haskell implementation

---
+++ src/Data/VirtualDOM.hs
@@ -0,0 +1,184 @@
+{-
+ - Original version (Copyright 2016 Bodil Stokke):
+ - https://github.com/bodil/purescript-vdom
+ -
+ - Porting (Copyright 2017 The Agile Monkeys S.L.)
+ -
+ - This is a port from PureScript to Haskell, using GHCJS
+ -
+ - This program is free software: you can redistribute it
+ - and/or modify it under the terms of the GNU Lesser General
+ - Public License as published by the Free Software Foundation,
+ - either version 3 of the License, or (at your option) any
+ - later version.
+ -
+ - This program is distributed in the hope that it will be
+ - useful, but WITHOUT ANY WARRANTY; without even the implied
+ - warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ - PURPOSE. See the GNU Lesser General Public License for
+ - more details.
+ -}
+module Ulmus.VirtualDOM where
+
+import           Foundation
+import           Foundation.Collection
+import           Control.Monad (when)
+import           GHCJS.Types
+
+import qualified Data.Map.Strict as Map
+import           Data.Map.Strict (Map)
+
+import qualified Ulmus.VirtualDOM.DOM as DOM
+
+type Props = Map String String
+
+data EventListener l = On String (JSVal -> IO ())
+
+data VNode l
+    = Element
+        { name :: String
+        , props :: Props
+        , listeners :: [EventListener l]
+        , children :: [VNode l]
+        }
+    | Text String
+
+h :: String -> Props -> [VNode l] -> VNode l
+h newName newProps = Element newName newProps []
+
+text :: String -> VNode l
+text = Text
+
+prop :: [(String, String)] -> Props
+prop = Map.fromList
+
+with :: VNode l-> [EventListener l] -> VNode l
+with (Text t) _ = Text t
+with e l = e { listeners = l }
+
+data DOM l = DOM
+  { createElement :: String -> IO l
+  , createElementNS :: String -> String -> IO l
+  , createTextNode :: String -> IO l
+  , replaceChild :: l -> l -> l -> IO ()
+  , removeChild :: l -> l -> IO ()
+  , appendChild :: l -> l -> IO ()
+  , childCount :: l -> IO Int
+  , childAt :: Int -> l -> IO (Maybe l)
+  , setTextContent :: String -> l -> IO ()
+  , setAttribute :: String -> String -> l -> IO ()
+  , removeAttribute :: String -> l -> IO ()
+  , addEventListener :: String -> (JSVal -> IO ()) -> l -> IO ()
+  }
+
+domAPI :: DOM DOM.Node
+domAPI = DOM
+  { createElement = DOM.createElement
+  , createElementNS = DOM.createElementNS
+  , createTextNode = DOM.createTextNode
+  , replaceChild = DOM.replaceChild
+  , removeChild = DOM.removeChild
+  , appendChild = DOM.appendChild
+  , childCount = DOM.childCount
+  , childAt = DOM.childAt
+  , setTextContent = DOM.setTextContent
+  , setAttribute = DOM.setAttribute
+  , removeAttribute = DOM.removeAttribute
+  , addEventListener = DOM.addEventListener
+  }
+
+newtype Elem = Elem JSVal
+
+createEl :: DOM l-> VNode l-> IO l
+createEl api (Text t) = createTextNode api t
+createEl api e = do
+    el <- createElement api (name e)
+    mapM_ (\(k, v) -> setAttribute api k v el) (Map.toList $ props e)
+    mapM_ (addListener api el) (listeners e)
+    mapM_ (createEl api >=> flip (appendChild api) el) (children e)
+    return el
+
+addListener :: DOM l-> l -> EventListener l-> IO ()
+addListener api target (On n handler) = addEventListener api n handler target
+
+changed :: VNode l-> VNode l-> Bool
+changed (Text t1) (Text t2) = t1 /= t2
+changed e1 e2 = name e1 /= name e2
+
+updateProps :: DOM l-> l -> Props -> Props -> IO ()
+updateProps api target old new =
+    mapM_ update (Map.keys (Map.union old new))
+  where
+    update key =
+        case (Map.lookup key old, Map.lookup key new) of
+            (Nothing, Just value) ->
+                setAttribute api key value target
+            (Just _, Nothing) ->
+                removeAttribute api key target
+            (Just prev, Just next) ->
+                when (prev /= next) (setAttribute api key next target)
+            (Nothing, Nothing) ->
+                return ()
+
+patch :: DOM l-> l -> Maybe (VNode l) -> Maybe (VNode l) -> IO ()
+patch api target' old' new' = patchIndexed api target' old' new' 0
+
+patchIndexed :: DOM l-> l -> Maybe (VNode l) -> Maybe (VNode l) -> Int -> IO ()
+patchIndexed _ _ Nothing Nothing _ = return ()
+
+patchIndexed api parent Nothing (Just new) _ = do
+  el <- createEl api new
+  appendChild api el parent
+
+patchIndexed api parent (Just _) Nothing index = do
+  child <- childAt api index parent
+  case child of
+    Just n -> removeChild api n parent
+    Nothing -> return ()
+
+patchIndexed api parent (Just (Text old)) (Just (Text new)) index =
+  when (old /= new) $ do
+    me <- childAt api index parent
+    maybe (return ()) (setTextContent api new) me
+
+patchIndexed api parent (Just old) (Just new) index = do
+  me' <- childAt api index parent
+  case me' of
+    Nothing -> return ()
+    Just me ->
+      if changed old new
+          then do
+            n <- createEl api new
+            replaceChild api n me parent
+          else do
+            case (old, new) of
+              (Element {props = oldProps}, Element {props = newProps}) ->
+                updateProps api me oldProps newProps
+              (_, _) -> return ()
+            walkChildren api me old new
+
+walkChildren :: DOM l -> l -> VNode l -> VNode l -> IO ()
+walkChildren api target old new =
+    if oldLength > newLength
+        then do
+            walkIndexes' [0 .. (newLength - 1)] -- walk up to last child of new
+            walkIndexes' [(oldLength - 1) .. newLength] -- delete children backwards from end
+        else
+            walkIndexes' [0 .. (newLength - 1)]
+  where
+    walkIndexes' =
+        mapM_ (\i' ->
+            let
+                i = Offset i'
+            in
+                patchIndexed api target (children old ! i) (children new ! i) i')
+    oldLength = fromIntegral $ toInteger $ length $ children old
+    newLength = fromIntegral $ toInteger $ length $ children new
+
+foreign import javascript unsafe "$r = document.body;"
+    js_body :: IO JSVal
+
+getBody :: IO DOM.Node
+getBody = do
+    putStrLn "Getting body"
+    DOM.Node <$> js_body
---
+++ src/Data/VirtualDOM/DOM.hs
@@ -0,0 +1,113 @@
+{-
+ - Copyright (c) 2017 The Agile Monkeys S.L. <hackers@theam.io>
+ -
+ - Licensed under the Apache License, Version 2.0 (the "License");
+ - you may not use this file except in compliance with the License.
+ - You may obtain a copy of the License at
+ -
+ -     http://www.apache.org/licenses/LICENSE-2.0
+ -
+ - Unless required by applicable law or agreed to in writing, software
+ - distributed under the License is distributed on an "AS IS" BASIS,
+ - WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ - See the License for the specific language governing permissions and
+ - limitations under the License.
+ -}
+module Ulmus.VirtualDOM.DOM where
+
+import Foundation
+import Foundation.Collection
+
+import GHCJS.Types
+import GHCJS.Foreign.Callback
+import qualified Data.JSString as JSString
+import qualified JavaScript.Array.Internal as JSArray
+import JavaScript.Array.Internal (JSArray)
+
+newtype Node = Node JSVal
+
+toJSString :: String -> JSString
+toJSString = JSString.pack . toList
+
+
+
+createElement :: String -> IO Node
+createElement name = Node <$> js_createElement (toJSString name)
+
+createElementNS :: String -> String -> IO Node
+createElementNS ns name = Node <$> js_createElementNS (toJSString ns) (toJSString name)
+
+createTextNode :: String -> IO Node
+createTextNode name = Node <$> js_createTextNode (toJSString name)
+
+replaceChild :: Node -> Node -> Node -> IO ()
+replaceChild (Node new) (Node old) (Node parent) = js_replaceChild new old parent
+
+removeChild :: Node -> Node -> IO ()
+removeChild (Node node) (Node parent) = js_removeChild node parent
+
+appendChild :: Node -> Node -> IO ()
+appendChild (Node node) (Node parent) = js_appendChild node parent
+
+childCount :: Node -> IO Int
+childCount (Node e) = do
+    children <- JSArray.toList <$> js_children e
+    let (CountOf l) = length children
+    return l
+
+childAt :: Int -> Node -> IO (Maybe Node)
+childAt idx (Node e) = do
+    children <- JSArray.toList <$> js_children e
+    return (Node <$> children ! Offset idx)
+
+setTextContent :: String -> Node -> IO ()
+setTextContent value (Node node) = js_setTextContent (toJSString value) node
+
+setAttribute :: String -> String -> Node -> IO ()
+setAttribute name value (Node node) = js_setAttribute (toJSString name) (toJSString value) node
+
+removeAttribute :: String -> Node -> IO ()
+removeAttribute name (Node node) = js_removeAttribute (toJSString name) node
+
+addEventListener :: String -> (JSVal -> IO ()) -> Node -> IO ()
+addEventListener eventName f (Node node) = do
+    cb <- asyncCallback1 f
+    js_addEventListener (toJSString eventName) cb node
+
+-- FFI
+
+foreign import javascript unsafe "document.createElement($1)"
+    js_createElement :: JSString -> IO JSVal
+
+foreign import javascript unsafe "document.createElementNS($1, $2)"
+    js_createElementNS :: JSString -> JSString -> IO JSVal
+
+foreign import javascript unsafe "document.createTextNode($1)"
+    js_createTextNode :: JSString -> IO JSVal
+
+foreign import javascript unsafe "$3.replaceChild($1, $2);"
+    js_replaceChild :: JSVal -> JSVal -> JSVal -> IO ()
+
+foreign import javascript unsafe "$2.removeChild($1);"
+    js_removeChild :: JSVal -> JSVal -> IO ()
+
+foreign import javascript unsafe "$2.appendChild($1);"
+    js_appendChild :: JSVal -> JSVal -> IO ()
+
+foreign import javascript unsafe "$r = $1.children;"
+    js_children :: JSVal -> IO JSArray
+
+foreign import javascript unsafe "$2.textContent = $1;"
+    js_setTextContent :: JSString -> JSVal -> IO ()
+
+foreign import javascript unsafe "$3.setAttribute($1, $2);"
+    js_setAttribute :: JSString -> JSString -> JSVal -> IO ()
+
+foreign import javascript unsafe "$2.removeAttribute($1);"
+    js_removeAttribute :: JSString -> JSVal -> IO ()
+
+foreign import javascript unsafe "$3.addEventListener($1, $2);"
+    js_addEventListener :: JSString -> Callback (JSVal -> IO()) -> JSVal -> IO ()
+
+--
+
